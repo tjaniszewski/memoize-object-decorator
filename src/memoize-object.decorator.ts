a05c15091ed79e785d7cef98e41f510249b9ce20
Tomasz Janiszewski <tjaniszewski@pgs-soft.com>
2018-06-29 13:17:02 +0200

    Types have been added and some unnecessary empty lines have been removed

--- src/memoize-object.decorator.ts
+++ src/memoize-object.decorator.ts
@@ -1,18 +1,17 @@
 import { memoize } from 'lodash';
 import { sha1 } from 'object-hash';
 
-
 export function MemoizeObject(): Function {
   return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor): PropertyDescriptor {
     if(descriptor === undefined) {
       descriptor = Object.getOwnPropertyDescriptor(target, key);
     }
 
-    const originalMethod = descriptor.value;
+    const originalMethod: Function = descriptor.value;
   
-    descriptor.value = memoize(function (...args) {
+    descriptor.value = memoize(function (...args: any[]) {
       return originalMethod.apply(this, args);
-    }, (...args)=> sha1(args));
+    }, (...args: any[])=> sha1(args));
     
     return descriptor;
   }
