15519a5e4f45f8a97e94baf85afbebf81f215a6a
Tomasz Janiszewski <tjaniszewski@pgs-soft.com>
2018-07-03 12:53:59 +0200

    Formatting fixes

--- src/memoize-object.decorator.ts
+++ src/memoize-object.decorator.ts
@@ -2,16 +2,16 @@ import { memoize } from 'lodash';
 import { sha1 } from 'object-hash';
 
 export function MemoizeObject(): Function {
-  return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor): PropertyDescriptor {
+  return function(target: Object, key: string | symbol, descriptor: PropertyDescriptor): PropertyDescriptor {
     if(descriptor === undefined) {
       descriptor = Object.getOwnPropertyDescriptor(target, key);
     }
 
     const originalMethod: Function = descriptor.value;
   
-    descriptor.value = memoize(function (...args: any[]) {
+    descriptor.value = memoize(function(...args: any[]) {
       return originalMethod.apply(this, args);
-    }, (...args: any[])=> sha1(args));
+    }, (...args: any[]) => sha1(args));
     
     return descriptor;
   }
